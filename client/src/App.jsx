7ccaca67ada320a633f70ad636e4ca84d812310c
siniiitsa <siniiitsa@gmail.com>
2019-08-23 19:34:40 +0300

    changes in cart component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 // eslint-disable-next-line no-unused-vars
-import React, { Fragment } from "react";
-import { Switch, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage/index";
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
     <Fragment>
       <Switch>
-        <Route exact path="/" component={MainPage} />
+        <Route exact path='/cart' component={Cart} />
+        <Route exact path='/' component={MainPage} />
       </Switch>
     </Fragment>
   );
