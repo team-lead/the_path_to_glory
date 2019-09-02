8d713091691a722a8fa3bd8dfe114bf17ae6b6cf
Oleksiy Simak <wwwoleksiy12345@gmail.com>
2019-09-02 07:13:11 +0300

    add footer component, fix colors, destructur. classes and use React.Fragment

--- client/src/components/MainPage/ShopByCategory/index.js
+++ client/src/components/MainPage/ShopByCategory/index.js
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ClothesImgItem from "../ClothesImgItem";
 import { classes } from "./style";
 
 const NewCollections = () => {
+  const {
+    collectionsContainer,
+    collectionsTitle,
+    collectionImgsBlock,
+    collectionItem,
+    mainCollectionImg,
+    сollectionImg
+  } = classes;
   return (
-    <>
-      <div className={classes.collectionsContainer}>
-        <p className={classes.collectionsTitle}>Shop by Category</p>
-        <div className={classes.collectionImgsBlock}>
-          <div className={classes.collectionItem}>
+    <Fragment>
+      <div className={collectionsContainer}>
+        <p className={collectionsTitle}>Shop by Category</p>
+        <div className={collectionImgsBlock}>
+          <div className={collectionItem}>
             <ClothesImgItem
               classes={classes}
               alt="BAG SHOP"
               src="https://images.unsplash.com/photo-1544816155-12df9643f363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-              className={classes.сollectionImg}
+              className={сollectionImg}
               text="BAG SHOP"
             />
             <ClothesImgItem
               classes={classes}
               alt="DRESS SHOP"
               src="https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80"
-              className={classes.сollectionImg}
+              className={сollectionImg}
               text="DRESS SHOP"
             />
             <ClothesImgItem
               classes={classes}
               alt="BELT SHOP"
               src="https://images.unsplash.com/photo-1485458895885-7139b8fd447a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-              className={classes.сollectionImg}
+              className={сollectionImg}
               text="BELT SHOP"
             />
           </div>
@@ -36,13 +44,13 @@ const NewCollections = () => {
               classes={classes}
               alt="SUNGLASSES SHOP"
               src="https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-              className={classes.mainCollectionImg}
+              className={mainCollectionImg}
               text="SUNGLASSES SHOP"
             />
           </div>
         </div>
       </div>
-    </>
+    </Fragment>
   );
 };
 export default NewCollections;
