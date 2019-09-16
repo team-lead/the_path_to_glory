e282d4dc7b723c8aa335b72357379dfe0526f581
Sergey Sinitsa <37211313+siniiitsa@users.noreply.github.com>
2019-09-16 20:39:56 +0300

    Cart (#21)
    * created header
    
    * created header
    
    * Delete yarn.lock
    
    * add yarn.lock to gitignore
    
    * added slider section
    
    * created search modal window
    
    * connect google fonts and add Collections section
    
    * add   section and chanche img
    
    * fix components path
    
    * fix structure
    
    * Update index.html
    
    * my changes
    
    * changes in cart component
    
    * finished cart summary component
    
    * added a basic cart product block
    
    * cart page hardcoded complete
    
    * container component and cart page
    
    * new changes from develop
    
    * added header to product pages
    
    * install missing dependencies
    
    * cart done
    
    * added dinamic products rendeting in cart
    
    * added setCheckoutTotal, romoveProductFromBusket functionalit and
    
    * added functionality to cart
    
    * removed unused button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,27 +3,25 @@ import React, { Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import Cart from './pages/Cart';
-import MyAccountPage from './pages/MyAccountPage'
+import MyAccountPage from './pages/MyAccountPage';
 import LoginPage from './pages/LoginPage';
 import Checkout from './pages/Checkout';
 import DetailPage from './pages/DetailPage/DetailPage';
 import ProductsPage from './pages/ProductsPage';
 
-
 const App = () => {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/myaccount/:id' component={MyAccountPage} />
-          <Route exact path='/:id' component={LoginPage} />
-          <Route exact path='/cart' component={Cart} />
-          <Route exact path='/' component={MainPage} />
-          <Route exact path='/cart/checkout' component={Checkout} />
-          <Route exact path = "/product-list/:category" component = {ProductsPage}/>
-          <Route exact path='/detail/:id' component={DetailPage} />
-
-        </Switch>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/myaccount/:id' component={MyAccountPage} />
+        <Route exact path='/:id' component={LoginPage} />
+        <Route exact path='/cart' component={Cart} />
+        <Route exact path='/' component={MainPage} />
+        <Route exact path='/cart/checkout' component={Checkout} />
+        <Route exact path='/product-list/:category' component={ProductsPage} />
+        <Route exact path='/detail/:id' component={DetailPage} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 export default App;
--- client/src/reducers/detailGoodReducer/detailReducer.js
+++ client/src/reducers/detailGoodReducer/detailReducer.js
@@ -1,16 +1,81 @@
-import { GET_GOODS_DETAIL } from "../../actions/detailGoodsAction"
+import {
+  GET_GOODS_DETAIL,
+  REMOVE_PRODUCT_FROM_CART,
+  SET_CHECKOUT_TOTAL,
+  DEC_CART_PRODUCT_QUANTITY,
+  INC_CART_PRODUCT_QUANTITY
+} from '../../actions/detailGoodsAction';
 
 const initialState = {
-    objectId: []
-}
+  objectId: [],
+  checkoutTotal: 0,
+  cart: [
+    {
+      id: 126,
+      images: [],
+      name: 'Gold Dress',
+      reference: '3294786 - 01',
+      price: 420,
+      quantity: 1,
+      color: 'gold',
+      size: 8
+    },
+    {
+      id: 121,
+      images: [],
+      name: 'Gold Dress',
+      reference: '3294786 - 01',
+      price: 420,
+      quantity: 1,
+      color: 'gold',
+      size: 8
+    },
+    {
+      id: 124,
+      images: [],
+      name: 'Gold Dress',
+      reference: '3294786 - 01',
+      price: 420,
+      quantity: 1,
+      color: 'gold',
+      size: 8
+    }
+  ]
+};
 
 export const activeGoodsReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_GOODS_DETAIL:
-            return {
-                ...state, objectId: action.payload
-            }
-        default: 
-            return state
+  switch (action.type) {
+    case GET_GOODS_DETAIL: {
+      return {
+        ...state,
+        objectId: action.payload
+      };
     }
-}
+    case REMOVE_PRODUCT_FROM_CART: {
+      const cart = state.cart.filter(product => product.id !== action.payload);
+      return { ...state, cart };
+    }
+    case SET_CHECKOUT_TOTAL: {
+      const checkoutTotal = action.payload;
+      return { ...state, checkoutTotal };
+    }
+    case INC_CART_PRODUCT_QUANTITY: {
+      const cart = state.cart.map(product => {
+        return product.id === action.payload
+          ? { ...product, quantity: product.quantity + 1 }
+          : { ...product };
+      });
+      return { ...state, cart };
+    }
+    case DEC_CART_PRODUCT_QUANTITY: {
+      const cart = state.cart.map(product => {
+        return product.id === action.payload && product.quantity > 1
+          ? { ...product, quantity: product.quantity - 1 }
+          : { ...product };
+      });
+      return { ...state, cart };
+    }
+    default:
+      return { ...state };
+  }
+};
