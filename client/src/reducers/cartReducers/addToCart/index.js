f02e7d3892cd46382e594fa8a15306868d415b74
vetalkonev <vetalkonev13@gmail.com>
2019-09-24 18:07:19 +0300

    addToCartButton

--- client/src/reducers/cartReducers/addToCart/index.js
+++ client/src/reducers/cartReducers/addToCart/index.js
@@ -1,31 +1,43 @@
 import {ADD_TO_CART} from "../../../actions/cartActions";
 
-const initialState =
-     [
-        {
-            category: "Category",
-            images: [],
-            name: "Леопардовые",
-            subCategory: "Труселя",
-            ref: String,
-            description: Array,
-            new: Boolean,
-            price: Number,
-            quantity: Number
-        }
-    ];
 
+// const initialState =
+//
+//      [
+//         {
+//             category: "Category",
+//             images: [],
+//             name: "Леопардовые",
+//             subCategory: "Труселя",
+//             ref: String,
+//             description: Array,
+//             new: Boolean,
+//             price: Number,
+//             quantity: Number
+//         }
+//     ];
+
+let initialState = {
+    inCart: {}
+};
 
 export function cartReducer(state = initialState, action) {
     switch (action.type) {
         case ADD_TO_CART: {
-            // логика добавления в корзину
+
+            const changedValues = {
+                ...state.inCart,
+                [action.payload.id]: action.payload,
+            };
+            window.localStorage.setItem('cart', JSON.stringify(state));
             return {
-                ...state, cart: {name: "Труселя оделись", id: action.id}
-            }
+                ...state,
+                loading: true,
+                inCart: changedValues,
+            };
         }
         default: {
-            return {...state};
+            return state
         }
     }
 }
