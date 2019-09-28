3fdc2585d167e640a15da86eaef896fd3ade5462
Oleksiy Simak <wwwoleksiy12345@gmail.com>
2019-09-28 11:47:45 +0300

    delete dropdown menu

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,13 +5,7 @@ import { NavLink } from "react-router-dom";
 import { classes } from "./style";
 import { SHOW_MODAL_WINDOW, searchGoods } from "../../actions/searchActions";
 import { setPrevPagePath } from "../../actions/detailGoodsAction";
-import {
-  SHOW_DROPDOWN_MENU,
-  HIDE_DROPDOWN_MENU
-} from "../../actions/dropDownMenuAction";
-import DropdownHeaderMenu from "../DropdownHeaderMenu";
 import ProductItemSearch from "../ProductItemSearch";
-import { Link } from "react-router-dom";
 
 class Header extends Component {
   constructor(props) {
@@ -32,9 +26,6 @@ class Header extends Component {
     const {
       searchModal,
       showSearchModal,
-      showDropdownMenu,
-      activeDropdownMenu,
-      hideDropdownMenu,
       searchResults,
       setPrevPagePath
     } = this.props;
@@ -45,17 +36,15 @@ class Header extends Component {
       navbarMenuItem,
       logoItem,
       headerActions,
-      headerActionsItemText,
       headerActionsItemImg,
       search,
       searchInput,
-      searchBtn,
-      dropdownMenuItem,
-      dropdownMenuItemGender,
       clearSearchBtn,
+      headerActionsItemText,
       searchBlock,
       productItemSearch,
-      resultCount
+      resultCount,
+      headerActionsItem
     } = classes;
 
     const searchResultsView = this.state.searchText ? (
@@ -91,203 +80,46 @@ class Header extends Component {
       </div>
     ) : null;
 
-    // const logoLink = window.location.pathname === "/" ? "#" : "/";
-
-    let showDropdownMenuItem = null;
-
-    // switch (activeDropdownMenu) {
-    //   case 0: {
-    //     showDropdownMenuItem = (
-    //       <DropdownHeaderMenu
-    //         title={'CATEGORIES'}
-    //         onMouseOver={() => showDropdownMenu(0)}
-    //         onMouseLeave={hideDropdownMenu}>
-    //         <div>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             New arrivals
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Shirts
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Coats
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Jackets
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Sweaters
-    //           </a>
-    //         </div>
-    //         <div>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Polos & Tees
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Jeans & Pants
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Outerwear
-    //           </a>
-    //         </div>
-    //       </DropdownHeaderMenu>
-    //     );
-    //     break;
-    //   }
-    //   case 1: {
-    //     showDropdownMenuItem = (
-    //       <DropdownHeaderMenu
-    //         title={'CATEGORIES'}
-    //         onMouseOver={() => showDropdownMenu(1)}
-    //         onMouseLeave={hideDropdownMenu}>
-    //         <div>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             New arrivals
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Dresses
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Knitwear
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Coats
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Jackets
-    //           </a>
-    //         </div>
-    //         <div>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Suits & Combined
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             T-shirts
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Jeans
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Skirts
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Underwear
-    //           </a>
-    //         </div>
-    //       </DropdownHeaderMenu>
-    //     );
-    //     break;
-    //   }
-    //   case 2: {
-    //     showDropdownMenuItem = (
-    //       <DropdownHeaderMenu
-    //         title={'CATEGORIES'}
-    //         onMouseOver={() => showDropdownMenu(2)}
-    //         onMouseLeave={hideDropdownMenu}>
-    //         <div>
-    //           <a
-    //             className={`${dropdownMenuItem} ${dropdownMenuItemGender}`}
-    //             href='#'>
-    //             For Woman
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Bags
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Waletts
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Belts
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Scarves & hats
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Sunglasses
-    //           </a>
-    //         </div>
-    //         <div>
-    //           <a
-    //             className={`${dropdownMenuItem} ${dropdownMenuItemGender}`}
-    //             href='#'>
-    //             For Man
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Bags
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Waletts, Card Cases
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Belts
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Ties & Cummerbunds
-    //           </a>
-    //           <a className={dropdownMenuItem} href='#'>
-    //             Sunglasses
-    //           </a>
-    //         </div>
-    //       </DropdownHeaderMenu>
-    //     );
-    //     break;
-    //   }
-    //   default: {
-    //   }
-    // }
-
     return (
       <>
         <header className={header}>
           <nav className={navbarMenu}>
-            <NavLink
-              to="/product-list/mens"
-              className={navbarMenuItem}
-              onMouseOver={() => showDropdownMenu(0)}>
+            <NavLink to="/product-list/mens" className={navbarMenuItem}>
               man
             </NavLink>
-            <NavLink
-              to="/product-list/womens"
-              className={navbarMenuItem}
-              onMouseOver={() => showDropdownMenu(1)}>
+            <NavLink to="/product-list/womens" className={navbarMenuItem}>
               woman
             </NavLink>
-            <NavLink
-              to="/product-list/accessories"
-              className={navbarMenuItem}
-              onMouseOver={() => showDropdownMenu(2)}>
+            <NavLink to="/product-list/accessories" className={navbarMenuItem}>
               accessory
             </NavLink>
           </nav>
-          <div onMouseOver={hideDropdownMenu}>
+          <div>
             <NavLink to="/" className={logoItem}>
               Originalité
             </NavLink>
           </div>
           <div className={headerActions}>
-            <NavLink onClick={searchModal} onMouseOver={hideDropdownMenu}>
+            <NavLink onClick={searchModal}>
               <i className={`fas fa-search ${headerActionsItemImg}`} />
-              <p className={classes.headerActionsItemText}>Search</p>
+              <p className={headerActionsItemText}>Search</p>
             </NavLink>
-            <NavLink to="/myaccount/main" className={classes.headerActionsItem}>
+            <NavLink to="/myaccount/main" className={headerActionsItem}>
               <i className={`far fa-user ${headerActionsItemImg}`} />
-              <p className={classes.headerActionsItemText}>My account</p>
+              <p className={headerActionsItemText}>My account</p>
             </NavLink>
             <NavLink
               to="/cart"
               onClick={() => {
                 setPrevPagePath(document.location.pathname);
               }}
-              className={classes.headerActionsItem}>
-              <i
-                className={`fas fa-shopping-bag ${classes.headerActionsItemImg}`}
-              />
-              <p className={classes.headerActionsItemText}>Shopping Bag</p>
+              className={headerActionsItem}>
+              <i className={`fas fa-shopping-bag ${headerActionsItemImg}`} />
+              <p className={headerActionsItemText}>Shopping Bag</p>
             </NavLink>
           </div>
         </header>
         {searchModalItem}
-        {showDropdownMenuItem}
       </>
     );
   }
@@ -296,8 +128,6 @@ class Header extends Component {
 const mapStateToProps = state => {
   return {
     showSearchModal: state.search.showSearchModal,
-    activeDropdownMenu: state.dropdownMenu.activeDropdownMenuItem,
-    dropDownMenuActive: state.dropdownMenu.dropDownMenuActive,
     searchResults: state.search.searchResults
   };
 };
@@ -305,8 +135,6 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     searchModal: () => dispatch({ type: SHOW_MODAL_WINDOW }),
-    showDropdownMenu: id => dispatch({ type: SHOW_DROPDOWN_MENU, payload: id }),
-    hideDropdownMenu: () => dispatch({ type: HIDE_DROPDOWN_MENU }),
     searchAction: searchKeywords => dispatch(searchGoods(searchKeywords)),
     setPrevPagePath: path => dispatch(setPrevPagePath(path))
   };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,14 @@
 import { combineReducers } from "redux";
 import { activeGoodsReducer } from "./detailGoodReducer/detailReducer";
-import { allGoods } from "./getGoodsReducer/index";
+import { allGoods } from "./getGoodsReducer";
 import searchReducer from "./mainPageReducers/searchReducer";
 import addToCartReducer from "./cartReducers/addToCart";
-import showDropdownMenuReducer from "./mainPageReducers/dropDownMenuReducer";
 import { allCategories } from "./getCategoriesReducer/index";
 
 const rootReducer = combineReducers({
   active: activeGoodsReducer,
   search: searchReducer,
   add: addToCartReducer,
-  dropdownMenu: showDropdownMenuReducer,
   allGoods: allGoods,
   allCategories
 });
