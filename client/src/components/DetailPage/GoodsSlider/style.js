85cebf83f550c1d4018a7b60e61aab2caa46ac99
Oleksiy Simak <wwwoleksiy12345@gmail.com>
2019-10-01 03:14:48 +0300

    add adaptive on detail page

--- client/src/components/DetailPage/GoodsSlider/style.js
+++ client/src/components/DetailPage/GoodsSlider/style.js
@@ -79,59 +79,12 @@ const goodsSlider = {
 
   "@media(min-width: 1260px)": {
     slider: {
-      width: "415px",
-
-      "& > ul.slick-dots ": {
-        position: "relative",
-        bottom: "30px"
-      },
-
-      "& > button.slick-arrow.slick-prev": {
-        left: "3px",
-        bottom: "20px"
-      },
-
-      "& > button.slick-arrow.slick-next": {
-        right: "3px",
-
-        "&::before": {
-          opacity: "0",
-          transition: ".5s"
-        }
-      },
-
-      "& > button.slick-arrow.slick-prev, .slick-next": {
-        zIndex: "1",
-        width: "30px",
-        top: "48%",
-
-        "&::before": {
-          color: "black",
-          left: "-24px",
-          bottom: "20px",
-          fontSize: "30px",
-          opacity: "0",
-          transition: ".5s"
-        }
-      },
-
-      "&:hover > button.slick-arrow.slick-prev": {
-        "&::before": {
-          opacity: "1"
-        }
-      },
-
-      "&:hover > button.slick-arrow.slick-next": {
-        "&::before": {
-          opacity: "1"
-        }
-      }
+      width: "415px"
     },
 
     imageSize: {
       height: "603px",
-      width: "415px",
-      objectFit: "cover"
+      width: "415px"
     }
   }
 };
