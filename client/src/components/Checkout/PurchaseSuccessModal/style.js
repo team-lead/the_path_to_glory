37f2ad11e44e8f01bd50a69df514364b83fb1889
Sergey Sinitsa <37211313+siniiitsa@users.noreply.github.com>
2019-10-02 17:03:25 +0300

    New cart (#62)
    * created header
    
    * created header
    
    * Delete yarn.lock
    
    * add yarn.lock to gitignore
    
    * added slider section
    
    * created search modal window
    
    * connect google fonts and add Collections section
    
    * add   section and chanche img
    
    * fix components path
    
    * fix structure
    
    * Update index.html
    
    * my changes
    
    * changes in cart component
    
    * finished cart summary component
    
    * added a basic cart product block
    
    * cart page hardcoded complete
    
    * container component and cart page
    
    * new changes from develop
    
    * added header to product pages
    
    * install missing dependencies
    
    * cart done
    
    * added dinamic products rendeting in cart
    
    * added setCheckoutTotal, romoveProductFromBusket functionalit and
    
    * added functionality to cart
    
    * removed unused button
    
    * mobile cart version done
    
    * adaptive cart done
    
    * added heador component to cart page
    
    * changed port to 8080
    
    * commit
    
    * changed cart to shoppingBag
    
    * added update purchase history
    
    * add products to cart
    
    * some last minute changes
    
    * header styles fixed
    
    * added date to cart objects
    
    * added header cart products counter
    
    * added header cart products counter
    
    * added link from cart product to detal page of that same product
    
    * button success and fail messages done
    
    * fixed bug with adding products with no color property to the basket
    
    * fixed a bug with overlapping sccess and fail messages
    
    * added badge and button animations
    
    * added adaptive footer
    
    * added animations
    
    * removed address book
    
    * added seach animation and botton animation
    
    * checkout modal done

--- client/src/components/Checkout/PurchaseSuccessModal/style.js
+++ client/src/components/Checkout/PurchaseSuccessModal/style.js
@@ -8,36 +8,46 @@ const small = '@media screen and (max-width: 790px)';
 const PurchaseSuccessModalStyles = {
   modal: {
     width: '500px',
-    height: '300px',
+    height: '550px',
     backgroundColor: 'black',
     position: 'absolute',
-    top: '30%',
+    top: '21%',
     left: '33%',
-    padding: '10px'
+    padding: '10px',
+    zIndex: '999'
+  },
+  check: {
+    color: 'white',
+    fontSize: '250px',
+    display: 'block'
   },
   text: {
     color: 'white',
-    fontSize: '30px'
+    fontSize: '30px',
+    marginBottom: '50px'
   },
   modalContainer: {
     fontFamily: 'Josefin Sans, sans-serif',
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     position: 'relative',
     width: '100%',
     height: '100%'
   },
-  closeBtn: {
-    position: 'absolute',
-    top: '0',
-    right: '0',
-    backgroundColor: 'transparent',
+  okBtn: {
+    marginTop: '50px',
+    padding: '23px 50px 13px',
+    backgroundColor: 'white',
+    textDecoration: 'none',
+    fontSize: '50px',
+    fontWeight: '700',
     border: 'none',
-    color: 'white',
+    color: 'black',
     cursor: 'pointer',
     '&:hover': {
-      textDecoration: 'underline'
+      backgroundColor: 'LIGHTGREEN',
     }
   }
 };
