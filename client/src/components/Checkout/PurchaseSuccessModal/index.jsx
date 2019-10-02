41969e5b7339f33794de020154f36361c30ead7e
siniiitsa <siniiitsa@gmail.com>
2019-10-02 16:23:00 +0300

    checkout modal done

--- client/src/components/Checkout/PurchaseSuccessModal/index.jsx
+++ client/src/components/Checkout/PurchaseSuccessModal/index.jsx
@@ -4,14 +4,15 @@ import { hideCheckoutModal } from '../../../actions/detailGoodsAction';
 import {connect} from 'react-redux'
 
 const PurchaseSuccessModal = ({hideCheckoutModal}) => {
-  const {modal, text, closeBtn, modalContainer} = classes;
+  const {modal, text, okBtn, modalContainer, check} = classes;
   return (
     <div className={modal}>
       <div className={modalContainer}>
         <p className={text}>Your payment is Succesfull!</p>
-        <button onClick={hideCheckoutModal} className={closeBtn}>
-          Close
-        </button>
+        <i class={`far fa-check-circle ${check}`}></i>
+        <a href='/' className={okBtn}>
+          ok
+        </a>
       </div>
     </div>
   );
